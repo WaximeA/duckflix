2078b06620005283aa0d7172c30e06f2fb45fe8d
WaximeA <aveline.maxime@gmail.com>
2018-04-29 21:51:43 +0200

    #watch : Add video in modal

--- src/pages/show-video/show-video.ts
+++ src/pages/show-video/show-video.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { HttpClient } from "@angular/common/http";
+
+const apiUrl='https://api.themoviedb.org';
 
 /**
  * Generated class for the ShowVideoPage page.
@@ -15,7 +18,21 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ShowVideoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  movieId: any;
+  videos: any = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController,  private http: HttpClient) {
+    this.movieId = this.navParams.get('movieId');
+    this.fetchAPI();
+  }
+
+  fetchAPI() {
+    this.http.get(`${apiUrl}/3/movie/${this.movieId}/videos?api_key=e47f7187bfd94a3b12ce8ca4ae282342`).subscribe(
+      data => {
+        this.videos = data['results'];
+      }, err => {
+        console.log("Une erreur s'est produite.");
+      });
   }
 
   dismiss() {
